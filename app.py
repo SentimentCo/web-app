85e542349ec409331ae7bb33de55a09705a204e6
Zhou You <yollyhideout@gmail.com>
2018-04-17 14:14:55 +0800

    update UI

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, IMAGES
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+
 import json
 import os
+import nltk
 
 app = Flask(__name__)
 photos = UploadSet('photos', IMAGES)
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
 configure_uploads(app, photos)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
 # english_bot.set_trainer(ChatterBotCorpusTrainer)
 # english_bot.train("chatterbot.corpus.english")
 
+def predict_sentiment(text, type):
+    def predict_sentence(sent):
+        word_count = len(nltk.word_tokenize(sent))
+        response = {
+        "sentiment": {
+            "label": ["anticipation", "Joy"],
+            "anticipation": 0.941,
+            "joy": 0.73,
+            "trust": 0.59,
+            "fear": 0.01,
+            "surprise": 0.042,
+            "sadness": 0.05,
+            "disgust": 0.03,
+            "anger": 0.01,
+        },
+        "wordCount": word_count
+        }
+        return response
+
+    if type == "document":
+        pred = predict_sentence(text)
+        return json.dumps([pred])
+
+    else:
+        sentences = nltk.sent_tokenize(text)
+        response = []
+        for sent in sentences:
+            response.append(predict_sentence(sent))
+        return json.dumps(response)
 
 @app.route("/")
 def home():
@@ -23,28 +51,15 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('messageText')
+    print(request.args)
+    type = request.args.get('type')
 
-    answer = str(english_bot.get_response(userText))
+    pred = predict_sentiment(userText, type)
     # score = 0.0
     # topic = 'shopping'
 
-    wordCount = len(answer.split(" "))
+    return json.dumps(pred)
 
-    # return json.dumps({'answer': answer, 'score':score, 'topic': topic})
-    return json.dumps( {
-         "sentiment": {
-          "label": ["anticipation", "Joy"],
-          "anticipation": 0.941,
-          "joy": 0.73,
-          "trust": 0.59,
-          "fear": 0.01,
-          "surprise":0.042,
-          "sadness":0.05,
-          "disgust":0.03,
-          "anger": 0.01,
-         },
-         "wordCount": wordCount
-        })
 
 
 @app.route('/upload', methods=['GET', 'POST'])
