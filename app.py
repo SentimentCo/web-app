668678ea977481894b7d94d83a9120830d80fdd9
Zhou You <yollyhideout@gmail.com>
2018-04-17 17:15:11 +0800

    add chart

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 import json
 import os
 import nltk
+import random
 
 app = Flask(__name__)
 photos = UploadSet('photos', IMAGES)
@@ -13,30 +14,48 @@ configure_uploads(app, photos)
 
 # english_bot.set_trainer(ChatterBotCorpusTrainer)
 # english_bot.train("chatterbot.corpus.english")
+def predict_sentence(sent):
+    word_count = len(nltk.word_tokenize(sent))
+    scores = get_score()
+    labels = get_labels(scores)
+    response = {
+        'sentiment': {
+            "anticipation": scores[0],
+            "joy": scores[1],
+            "trust": scores[2],
+            "fear": scores[3],
+            "surprise": scores[4],
+            "sadness": scores[5],
+            "disgust": scores[6],
+            "anger": scores[7],
+        },
+        'label': labels,
+        "wordCount": word_count,
+        "sent": sent
+    }
+    return response
+
+def get_score():
+    l = []
+    for i in range(8):
+        l.append(random.random())
+    return l
+
+def get_labels(scores):
+    labels = ["anticipation", "joy", "trust", "fear", "surprise", "sadness", "disgust", "anger"];
+    answers = []
+    for i, s in enumerate(scores):
+        if i > 0.7:
+            answers.append(labels[i])
+    return answers
+
+
 
 def predict_sentiment(text, type):
-    def predict_sentence(sent):
-        word_count = len(nltk.word_tokenize(sent))
-        response = {
-        "sentiment": {
-            "label": ["anticipation", "Joy"],
-            "anticipation": 0.941,
-            "joy": 0.73,
-            "trust": 0.59,
-            "fear": 0.01,
-            "surprise": 0.042,
-            "sadness": 0.05,
-            "disgust": 0.03,
-            "anger": 0.01,
-        },
-        "wordCount": word_count
-        }
-        return response
-
     if type == "document":
         pred = predict_sentence(text)
+        print(pred)
         return json.dumps([pred])
-
     else:
         sentences = nltk.sent_tokenize(text)
         response = []
@@ -51,14 +70,11 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('messageText')
-    print(request.args)
     type = request.args.get('type')
-
+    print(type)
     pred = predict_sentiment(userText, type)
-    # score = 0.0
-    # topic = 'shopping'
 
-    return json.dumps(pred)
+    return pred
 
 
 
